88074725fc952d0c9583700ed1d3d64182372d9a
Wenhao Ji <predator.ray@gmail.com>
2022-04-08 21:32:24 +0800

    Fixed eslint issues and upgraded dependencies

--- src/js/Cheer.js
+++ src/js/Cheer.js
@@ -1,185 +1,182 @@
-import Peer from "Peer";
-import {getLogger} from "SimpleLogger";
+import Peer from 'peerjs';
+import { getLogger } from './SimpleLogger';
 
 class Attendee {
+  static _log = getLogger(Attendee);
 
-    static log = getLogger(Attendee);
+  peerId;
 
-    peerId;
+  _peer;
+  _listeners = {
+    open: [],
+    close: []
+  };
 
-    _peer;
-    _listeners = {
-        open: [],
-        close: []
-    };
+  constructor(peerSettings) {
+    Attendee._log.debug(`The PeerJS settings: ${JSON.stringify(peerSettings)}`);
+    this._peer = new Peer(peerSettings);
+    Attendee._log.info(`Using the PeerJS server: ${peerSettings?.host || 'default'})`);
 
-    constructor(peerSettings) {
-        log.debug(`The PeerJS settings: ${JSON.stringify(peerSettings)}`);
-        this._peer = new Peer(peerSettings);
-        log.info(`Using the PeerJS server: ${peerSettings?.host || "default"})`);
+    const attendeeSelf = this;
+    this._peer.on('open', (peerId) => {
+      Attendee._log.info(`Connected to the PeerJS server. (peerId = ${peerId})`);
+      attendeeSelf.peerId = peerId;
+      for (const listener of attendeeSelf._listeners.open) {
+        listener.call(attendeeSelf, peerId);
+      }
+    });
 
-        const attendeeSelf = this;
-        this._peer.on("open", peerId => {
-            log.info(`Connected to the PeerJS server. (peerId = ${peerId})`);
-            attendeeSelf.peerId = peerId;
-            for (const listener of attendeeSelf._listeners.open) {
-                listener.call(attendeeSelf, peerId);
-            }
-        });
+    this._peer.on('close', () => {
+      for (const listener of attendeeSelf._listeners.close) {
+        listener.call(attendeeSelf);
+      }
+    });
+  }
 
-        this._peer.on("close", () => {
-            for (const listener of attendeeSelf._listeners.close) {
-                listener.call(attendeeSelf);
-            }
-        });
+  on(event, fn) {
+    if (!event) {
+      throw new Error('Event type is required.');
     }
-
-    on(event, fn) {
-        if (!event) {
-            throw new Error("Event type is required.");
-        }
-        if (!fn) {
-            throw new Error("Listener is required.");
-        }
-        const eventListeners = this._listeners[event];
-        if (!eventListeners) {
-            this._listeners[event] = [];
-        }
-        this._listeners[event].push(fn);
+    if (!fn) {
+      throw new Error('Listener is required.');
     }
-
-    off(event, fn) {
-        if (!event) {
-            throw new Error("Event type is required.");
-        }
-        if (!fn) {
-            this._listeners[event] = [];
-        } else {
-            const eventListeners = this._listeners[event];
-            if (!eventListeners) {
-                return;
-            }
-            this._listeners[event] = this._listeners[event].filter(e => e !== fn);
-        }
+    const eventListeners = this._listeners[event];
+    if (!eventListeners) {
+      this._listeners[event] = [];
     }
+    this._listeners[event].push(fn);
+  }
 
-    close() {
-        this._peer.close();
-        this._listeners = {};
+  off(event, fn) {
+    if (!event) {
+      throw new Error('Event type is required.');
     }
+    if (!fn) {
+      this._listeners[event] = [];
+    } else {
+      const eventListeners = this._listeners[event];
+      if (!eventListeners) {
+        return;
+      }
+      this._listeners[event] = this._listeners[event].filter(e => e !== fn);
+    }
+  }
+
+  close() {
+    this._peer.close();
+    this._listeners = {};
+  }
 }
 
 export class Host extends Attendee {
+  static _log = getLogger(Host);
 
-    static log = getLogger(Host);
+  _guests = [];
 
-    _guests = [];
+  constructor(peerSettings) {
+    super(peerSettings);
 
-    constructor(peerSettings) {
-        super(peerSettings);
+    this._listeners.join = [];
+    this._listeners.message = [];
+    this._listeners.leave = [];
 
-        this._listeners.join = [];
-        this._listeners.message = [];
-        this._listeners.leave = [];
-
-        const hostSelf = this;
-        this._peer.on("connection", guest => {
-            log.info(`A guest peer connected: ${JSON.stringify(guest)}`);
-            const releaseGuestConnection = () => {
-                guest.disconnect();
-                hostSelf._guests = hostSelf._guests.filter(e => e !== guest);
-                for (const listener of hostSelf._listeners.leave) {
-                    listener.call(hostSelf, guest);
-                }
-            };
-            guest.on("close", releaseGuestConnection);
-            guest.on("disconnect", releaseGuestConnection);
-            hostSelf._guests.push(guest);
-
-            hostSelf._guests = hostSelf._guests.filter(e => e !== guest);
-            for (const listener of hostSelf._listeners.join) {
-                listener.call(hostSelf, guest);
-            }
-
-            guest.on("data", data => {
-                for (const listener of hostSelf._listeners.message) {
-                    listener.call(hostSelf, guest, data);
-                }
-            });
-        });
-    }
-
-    kick(target) {
-        for (const guest of this._guests) {
-            if (guest === target) {
-                target.close();
-                return;
-            }
+    const hostSelf = this;
+    this._peer.on('connection', (guest) => {
+      Host._log.info(`A guest peer connected: ${JSON.stringify(guest)}`);
+      const releaseGuestConnection = () => {
+        guest.disconnect();
+        hostSelf._guests = hostSelf._guests.filter(e => e !== guest);
+        for (const listener of hostSelf._listeners.leave) {
+          listener.call(hostSelf, guest);
         }
-        log.warn("The guest does not exist.");
-    }
+      };
+      guest.on('close', releaseGuestConnection);
+      guest.on('disconnect', releaseGuestConnection);
+      hostSelf._guests.push(guest);
 
-    broadcast(message) {
-        for (const guest of this._guests) {
-            guest.send(message);
-        }
-    }
+      hostSelf._guests = hostSelf._guests.filter(e => e !== guest);
+      for (const listener of hostSelf._listeners.join) {
+        listener.call(hostSelf, guest);
+      }
 
-    close() {
-        for (const guest of this._guests) {
-            guest.close();
+      guest.on('data', (data) => {
+        for (const listener of hostSelf._listeners.message) {
+          listener.call(hostSelf, guest, data);
         }
-        super.close();
+      });
+    });
+  }
+
+  kick(target) {
+    for (const guest of this._guests) {
+      if (guest === target) {
+        target.close();
+        return;
+      }
     }
+    Host._log.warn('The guest does not exist.');
+  }
+
+  broadcast(message) {
+    for (const guest of this._guests) {
+      guest.send(message);
+    }
+  }
+
+  close() {
+    for (const guest of this._guests) {
+      guest.close();
+    }
+    super.close();
+  }
 }
 
 export class Guest extends Attendee {
+  static _log = getLogger(Guest);
 
-    static log = getLogger(Guest);
+  _hostPeerId;
+  _hostConnection;
 
-    _hostPeerId;
-    _hostConnection;
+  constructor(hostPeerId, peerSettings) {
+    super(peerSettings);
 
-    constructor(hostPeerId, peerSettings) {
-        super(peerSettings);
+    if (!hostPeerId) {
+      throw new Error('hostPeerId is required');
+    }
 
-        if (!hostPeerId) {
-            throw new Error("hostPeerId is required");
+    this._listeners.join = [];
+    this._listeners.message = [];
+    this._listeners.leave = [];
+
+    this._hostPeerId = hostPeerId;
+
+    const guestSelf = this;
+    this.on('open', () => {
+      guestSelf._hostConnection = guestSelf._peer.connect(hostPeerId);
+      guestSelf._hostConnection.on('open', () => {
+        Guest._log.info(`Connected to the host. (peerId = ${hostPeerId})`);
+
+        for (const listener of guestSelf._listeners.join) {
+          listener.call(guestSelf);
         }
 
-        this._listeners.join = [];
-        this._listeners.message = [];
-        this._listeners.leave = [];
-
-        this._hostPeerId = hostPeerId;
-
-        const guestSelf = this;
-        this.on("open", () => {
-            guestSelf._hostConnection = guestSelf._peer.connect(hostPeerId);
-            guestSelf._hostConnection.on("open", () => {
-                log.info(`Connected to the host. (peerId = ${hostPeerId})`);
-
-                for (const listener of guestSelf._listeners.join) {
-                    listener.call(guestSelf);
-                }
-
-                guestSelf._hostConnection.on("data", data => {
-                    log.debug(`Received data from the host: ${JSON.stringifydata}`);
-                    for (const listener of guestSelf._listeners.message) {
-                        listener.call(guestSelf, data);
-                    }
-                });
-            });
-            guestSelf._hostConnection.on("close", () => {
-                for (const listener of guestSelf._listeners.leave) {
-                    listener.call(guestSelf);
-                }
-            });
+        guestSelf._hostConnection.on('data', (data) => {
+          Guest._log.debug(`Received data from the host: ${JSON.stringifydata}`);
+          for (const listener of guestSelf._listeners.message) {
+            listener.call(guestSelf, data);
+          }
         });
-    }
+      });
+      guestSelf._hostConnection.on('close', () => {
+        for (const listener of guestSelf._listeners.leave) {
+          listener.call(guestSelf);
+        }
+      });
+    });
+  }
 
-    close() {
-        this._hostConnection.close();
-        super.close();
-    }
+  close() {
+    this._hostConnection.close();
+    super.close();
+  }
 }
--- src/js/SimpleLogger.js
+++ src/js/SimpleLogger.js
@@ -1,108 +1,106 @@
 const ROOT_LOGGER_NAME = 'ROOT';
 
 const Levels = {
-    ALL: 0,
-    DEBUG: 20,
-    INFO: 40,
-    WARN: 60,
-    ERROR: 80,
-    OFF: 999
+  ALL: 0,
+  DEBUG: 20,
+  INFO: 40,
+  WARN: 60,
+  ERROR: 80,
+  OFF: 999
 };
 
 const logSettings = {
-    level: Levels.WARN,
-    appender: null
+  level: Levels.WARN,
+  appender: null
 };
 
 class ConsoleAppender {
+  #console;
 
-    #console;
+  constructor(csl) {
+    this.#console = csl || console;
+  }
 
-    constructor(csl) {
-        this.#console = csl ? csl : console;
-    }
-    
-    print(msg) {
-        this.#console.log(msg);
-    }
+  print(msg) {
+    this.#console.log(msg);
+  }
 }
 
 class Logger {
+  #loggerName;
+  #appender;
 
-    #loggerName;
-    #appender;
- 
-    constructor(loggerName, appender) {
-        this.#loggerName = loggerName || ROOT_LOGGER_NAME;
-        this.#appender = appender || new ConsoleAppender(console);
-    }
+  constructor(loggerName, appender) {
+    this.#loggerName = loggerName || ROOT_LOGGER_NAME;
+    this.#appender = appender || new ConsoleAppender(console);
+  }
 
-    _log(log) {
-        if (!log.msg) {
-            return;
-        }
-        const logLevel = log.level || Levels.INFO;
-        if (logSettings.level > logLevel) {
-            return;
-        }
-        const ts = log.timestamp || Date.now();
-        const line = new Date(ts).toISOString() + " [" + logLevel + "] " + loggerName + " - " + log.msg;
-        this.#appender.print(line);
+  _log(log) {
+    if (!log.msg) {
+      return;
     }
+    const logLevel = log.level || Levels.INFO;
+    if (logSettings.level > logLevel) {
+      return;
+    }
+    const ts = log.timestamp || Date.now();
+    const line = `${new Date(ts).toISOString()} [${logLevel}] ${this.#loggerName} - ${log.msg}`;
+    this.#appender.print(line);
+  }
 
-    debug(msg) {
-        this._log({
-            level: Levels.DEBUG,
-            messsage: msg
-        });
-    }
+  debug(msg) {
+    this._log({
+      level: Levels.DEBUG,
+      messsage: msg
+    });
+  }
 
-    info(msg) {
-        this._log({
-            level: Levels.INFO,
-            messsage: msg
-        });
-    }
+  info(msg) {
+    this._log({
+      level: Levels.INFO,
+      messsage: msg
+    });
+  }
 
-    warn(msg) {
-        this._log({
-            level: Levels.WARN,
-            messsage: msg
-        });
-    }
+  warn(msg) {
+    this._log({
+      level: Levels.WARN,
+      messsage: msg
+    });
+  }
 
-    error(msg) {
-        this._log({
-            level: Levels.ERROR,
-            messsage: msg
-        });
-    }
+  error(msg) {
+    this._log({
+      level: Levels.ERROR,
+      messsage: msg
+    });
+  }
 }
 
 export default {
-    getLogger: function (logger) {
-        let loggerName;
-        if (!logger) {
-            loggerName = ROOT_LOGGER_NAME;
-        } else if (typeof logger === 'string' || logger instanceof String) {
-            loggerName = logger;
-        } else if (logger.name) {
-            loggerName = logger.name;
-        } else {
-            throw new Error("The logger must have a name.");
-        }
-        return new Logger(loggerName, logSettings.appender);
-    },
-
-    setLevel: function (level) {
-        const levelNo = Levels[level];
-        if (!levelNo) {
-            throw new Error("The level '" + level + "' does not exist.");
-        }
-        logSettings.level = levelNo;
-    },
-    
-    setAppender: function (appender) {
-        logSettings.appender = appender;
+  getLogger(logger) {
+    let loggerName;
+    if (!logger) {
+      loggerName = ROOT_LOGGER_NAME;
+    } else if (typeof logger === 'string' || logger instanceof String) {
+      loggerName = logger;
+    } else if (logger.name) {
+      loggerName = logger.name;
+    } else {
+      throw new Error('The logger must have a name.');
     }
+    return new Logger(loggerName, logSettings.appender);
+  },
+
+  setLevel(level) {
+    const levelNo = Levels[level];
+    if (!levelNo) {
+      throw new Error(`The level '${level}' does not exist.`);
+    }
+    logSettings.level = levelNo;
+  },
+
+  setAppender(appender) {
+    logSettings.appender = appender;
+  }
 };
