d727d5d31886661c22a56080d3284cc5baf7ba66
Luna <luleinju9476@gmail.com>
2022-05-14 10:28:18 -0700

    Fix duplicate body bug

--- src/App.js
+++ src/App.js
@@ -54,35 +54,35 @@ export default function App() {
     const peerId = useRef(null)
 
     return (
-        <div className="App">
-            <body>
-                <header>
-                    <img class="logo" src="cheers-64x64.png" />
-                    <h1>Cheers!</h1>
-                    <h2>Watch videos with friends.</h2>
-                </header>
+        <>
+            <header>
+                <img class="logo" src="cheers-64x64.png" />
+                <h1>Cheers!</h1>
+                <h2>Watch videos with friends.</h2>
+            </header>
 
-                <div class="content">
-                    <div class="content-row">
-                        <button id="host" onClick={onHost}>Host</button>
-                    </div>
-                    <div class="content-divider">
-                        <span>or</span>
-                    </div>
-                    <div class="content-row">
-                        <input id="peerId" type="text" placeholder="Host ID" ref={peerId}/>
-                        <button id="join" onClick={onJoin}>Join</button>
-                    </div>
+            <div class="content">
+                <div class="content-row">
+                    <button id="host" onClick={onHost}>Host</button>
                 </div>
-                <footer>
-                    <a target="_blank" href="https://github.com/predatorray">About</a>
-                    &middot;
-                    <a target="_blank" href="https://github.com/predatorray">Report</a>
-                    &middot;
-                    <a target="_blank" href="https://github.com/predatorray">Author</a>
-                </footer>
-            </body>
-        </div>
+                <div class="content-divider">
+                    <span>or</span>
+                </div>
+                <div class="content-row">
+                    <input id="peerId" type="text" placeholder="Host ID" ref={peerId} />
+                    <button id="join" onClick={onJoin}>Join</button>
+                </div>
+            </div>
+            <footer>
+                <a target="_blank" href="https://github.com/predatorray">About</a>
+                &middot;
+                <a target="_blank" href="https://github.com/predatorray">Report</a>
+                &middot;
+                <a target="_blank" href="https://github.com/predatorray">Author</a>
+            </footer>
+
+        </>
+
     );
 }
 
